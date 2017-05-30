e7dd8e93f599e1acfc29df5723b7dbf4d39c5c9f
Agustin Fiori <drcrow@gmail.com>
2017-05-29 21:15:29 -0300

    lista de populares
    Se ven los populares pero hay un error en algun lado

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,7 +2,9 @@ var axios = require('axios');
 
 module.exports = {
 	fetchPopularRepos: function(language){
-		var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+		var theURI = 'https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories';
+		console.log(theURI);
+		var encodedURI = window.encodeURI(theURI);
 		return axios.get(encodedURI)
 			.then(function(response){
 				return(response.data.items);
